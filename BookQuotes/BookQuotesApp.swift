a2646e054b42821f96608f169bee942c53761c5b
Joshua Söhn <joshua.soehn@me.com>
2026-02-01 18:15:23 +0100

    Initial Commit

---
+++ BookQuotes/BookQuotesApp.swift
@@ -0,0 +1,32 @@
+//
+//  BookQuotesApp.swift
+//  BookQuotes
+//
+//  Created by Joshua Söhn on 01.02.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookQuotesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
